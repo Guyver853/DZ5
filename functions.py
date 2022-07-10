681f59d02d4720d8279499583a3e9898beeb9896
Guyver853 <giper06@yandex.ru>
2022-07-10 20:24:28 +0300

    Created a file manager

---
+++ functions.py
@@ -0,0 +1,63 @@
+import os
+import shutil
+
+def create_folder(folder_name): # - создать папку
+    if not os.path.exists(folder_name): #  если не существует  файл (или папка)
+        os.mkdir(folder_name)
+        return 'Папка создана'
+    else:
+        return 'Папка с таким именем уже существует - пожалуйста, придумайте другое имя!'
+
+
+def delete_file_folder(): # - удалить (файл/папку)
+    del_name = input('Введите имя удаляемого файла или папки:')
+    while os.path.exists(del_name) != True:
+        print('Такого файла/папки не существует, введите другое название!')
+        del_name = input('Введите имя удаляемого файла или папки:')
+    if os.path.exists(del_name):
+        if os.path.isdir(del_name):
+            os.rmdir(del_name) # удаляем папку
+            return 'Папка удалена!'
+        elif os.path.isfile(del_name):
+            os.remove(del_name) # удаляем файл
+            return 'Файл удален!'
+    else:
+        return 'Выбранные файл или папка не удалены!'
+
+
+
+
+
+
+def copy_file_folder(): # - копировать (файл/папку)
+    do = input('Введите название  файла или папки для копирования: ')
+    while os.path.exists(do) != True:
+        print('Такого файла/папки не существует, введите другое название!')
+        do = input('Введите название  файла или папки для копирования: ')
+    posle = input('Введите новое название для копии файла(папки): ')
+    while do == posle:
+        print('Имена должны быть разными! Введите оригинальное название для нового файла/папки.')
+        posle = input('Введите новое название для копии файла(папки): ')
+    while os.path.exists(posle) == True :
+            print('Объект с таким именем уже существует. Введите другое имя для нового файла/папки')
+            posle = input('Введите новое название для копии файла(папки): ')
+    shutil.copy(do, posle) # Копируем файл или папку с помощью функции copy() модуля shutil
+    if os.path.exists(posle):
+        print('Объект успешно скопирован')
+    else:
+        print('Невозможно скопировать объект!')
+
+
+def view_content(smena): # - смена рабочей директории (*необязательный пункт)
+    if os.path.exists(smena): # После ввода - проверка, существует ли данная директория
+        os.chdir(smena) # Смена
+        return 'Вы перешли в директорию: os.getcwd()'
+    else:
+        return 'Такой директории не существует! Попробуйте еще раз.'
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,55 @@
+import sys
+import glob2
+
+from functions import *
+from bank_account import *
+from victory import *
+
+if __name__ == '__main__':
+
+    while True:
+        print('\n1. Создать папку')
+        print('2. Удалить (файл/папку)')
+        print('3. Копировать (файл/папку)')
+        print('4. Просмотр содержимого рабочей директории')
+        print('5. Посмотреть только папки')
+        print('6. Посмотреть только файлы')
+        print('7. Просмотр информации об операционной системе')
+        print('8. Создатель этой программы')
+        print('9. Мой банковский счет ')
+        print('10. Викторина "Дни рождения"')
+        print('11. Смена рабочей директории')
+        print('12. Найти все csv файлы, которые есть в этой директории')
+        print('0. Выход')
+
+        menu = input('Выберите цифру пункта меню: ')
+        if menu == '1':
+            print(create_folder(input('Введите название папки: ')))
+        elif menu == '2':
+            delete_file_folder()
+        elif menu == '3':
+            copy_file_folder()
+        elif menu == '4':
+            print(os.listdir())
+        elif menu == '5':
+            print([object for object in os.listdir() if os.path.isdir(object)]) #  Получаем список всех файлов и папок, а потом просто отсеиваем из них папки
+        elif menu == '6':
+            print([object for object in os.listdir() if os.path.isfile(object)]) #  Получаем список всех файлов и папок, а потом просто отсеиваем из них файлы
+        elif menu == '7':
+            print(os.name)
+            print(sys.platform)
+        elif menu == '8':
+            print('$$$ Creator of the program Igor Chernov $$$')
+        elif menu == '9':
+            bank_account()
+        elif menu == '10':
+            victory()
+        elif menu == '11':
+            print(view_content(input('Введите путь к новой директории: ')))
+        elif menu == '12':
+            for files in glob2.glob("*.csv"):
+                print(files)
+        elif menu == '0':
+            break
+        else:
+            print('\nНеверный пункт меню\n')
