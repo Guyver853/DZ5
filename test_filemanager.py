9053ef1df5eb759ab0d027dda03c693c76760dfd
Guyver853 <giper06@yandex.ru>
2022-07-24 19:25:15 +0300

    Доработаны файлы main.py и functions.py, test_filemanager.py и bank_account.py. А также добавлен для тестирования удаления файлик file.txt

--- test_filemanager.py
+++ test_filemanager.py
@@ -1,9 +1,10 @@
-from functions import create_folder, delete_file_folder, view_content
-from os import path, mkdir, rmdir
+from functions import create_folder, delete_file_folder, view_content, save_listdir
+from os import path, mkdir, rmdir, getcwd
 import os
+import json
 
 
-def test_make_directory():
+def test_create_folder():
     assert create_folder('papochka') == 'Папка создана', 'Ошибка!'
     assert path.exists('papochka'), 'Ошибка!'
 
@@ -17,6 +18,16 @@ def test_delete_file_folder():
 def test_view_content():
     # new_d = os.chdir(r'C:\Users\user\PycharmProjects\Phunkcii')
     assert view_content(r'C:\Users\user\PycharmProjects\Phunkcii') ==  os.getcwd(), 'Ошибка!'
+
+def test_save_listdir():
+    if not path.exists(path.join(getcwd(), 'create')):
+        mkdir('create')
+    assert save_listdir() == 'Содержимое директории успешно записано в файл listdir', 'Failed!'
+    if path.exists('listdir'):
+        with open('listdir', 'r', encoding='utf-8') as f:
+            f_listdir = json.load(f)
+    assert 'create' in f_listdir['dirs'], 'Не верно!'
+    rmdir('create')
 
 
 
